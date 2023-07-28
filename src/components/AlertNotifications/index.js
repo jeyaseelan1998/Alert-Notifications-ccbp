b3cc5d695c086dbab743226003bb09bbd7129241
jeyaseelan windows 10 <jeyaseelan.work@gmail.com>
2023-07-28 16:09:46 +0530

    codes updated

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,1 +1,40 @@
-// Write your code here
+import Notification from '../Notification'
+import { AiFillCheckCircle } from 'react-icons/ai'
+import { RiErrorWarningFill } from 'react-icons/ri'
+import { MdWarning, MdInfo } from 'react-icons/md'
+
+import './index.css'
+
+const AlertNotifications = () => {
+    return (
+        <div className='alert-notifications-container'>
+            <h1 className='main-heading'>Alert Notifications</h1>
+            <ul className='alert-notifications-list'>
+                <Notification>
+                    <AiFillCheckCircle className='notification-icon success' />
+                    <h1 className='notification-heading success'>Success</h1>
+                    <p className='notification-description'>You can access all the files in the folder</p>
+                </Notification>
+                <Notification>
+                    <RiErrorWarningFill className='notification-icon error' />
+                    <h1 className='notification-heading error'>Error</h1>
+                    <p className='notification-description'>Sorry, you are not authorized to have access to
+                        delete the file</p>
+                </Notification>
+                <Notification>
+                    <MdWarning className='notification-icon warning' />
+                    <h1 className='notification-heading warning'>Warning</h1>
+                    <p className='notification-description'>Viewers of this file can see comments and
+                        suggestions</p>
+                </Notification>
+                <Notification>
+                    <MdInfo className='notification-icon info' />
+                    <h1 className='notification-heading info'>Info</h1>
+                    <p className='notification-description'>Anyone on the internet can view these files</p>
+                </Notification>
+            </ul>
+        </div>
+    )
+}
+
+export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,1 +1,20 @@
-// Write your code here
+import {GrFormClose} from 'react-icons/gr'
+import './index.css'
+
+const Notification = props => {
+    const {children} = props
+    const [NotificationIcon, Heading, Description] = children
+    console.log(children);
+    return(
+        <li className='notification-container'>
+            {NotificationIcon}
+            <div className='notification-details'>
+                {Heading}
+                {Description}
+            </div>
+            <GrFormClose className='close-icon'/>
+        </li>
+    )
+}
+
+export default Notification
